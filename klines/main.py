2b07789afd414aa50ae1d872e687517b7c58560b
Sparsh Kumar <sparshkumar@Sparshs-MacBook-Pro.local>
2025-05-10 20:05:45 +0530

    removed requirements.txt from individual folders, kept it in root.

--- klines/main.py
+++ klines/main.py
@@ -1,18 +1,24 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from klines import SyncKLines
 
 load_dotenv()
 
-
 def main():
-  asset = 'BTC/USDT'
+
+  parser = argparse.ArgumentParser(description="Sync Klines (OHLCV) Data to MongoDB")
+  parser.add_argument('--ticker', type=str, required=True, help='Asset ticker symbol (e.g., BTC/USDT)')
+  parser.add_argument('--timeframe', type=str, default='1m', help='Timeframe (e.g., 1m, 5m, 15m, 1h, 1d) supported by CCXT')
+  
+  args = parser.parse_args()
+
   config = {
     'mongoServerName': os.getenv('MONGODB_SERVER'),
     'mongoDatabaseName': os.getenv('MONGO_DATABASE'),
-    'mongoCollectionName': asset.lower(),
-    'ticker': asset,
-    'timeframe': '1m',
+    'mongoCollectionName': args.ticker.replace('/', '_').lower(),
+    'ticker': args.ticker.lower(),
+    'timeframe': args.timeframe,
   }
   syncKLines = SyncKLines(config)
   syncKLines.fetchOhlcvData()
